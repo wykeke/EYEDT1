de9010b52321c8d8beb17d313a75f5b05ac2ac01
wykeke <2565556263@qq.com>
2021-03-18 20:39:17 +0800

    首页

--- app/src/main/java/com/example/eyedt/ui/fragment/Eye_Fragment.kt
+++ app/src/main/java/com/example/eyedt/ui/fragment/Eye_Fragment.kt
@@ -5,19 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager.widget.ViewPager
 import com.example.eyedt.R
+import com.example.eyedt.ui.adapter.PageAdapter
 import com.google.android.material.tabs.TabLayout
+import java.util.ArrayList
 
 class Eye_Fragment : Fragment() {
+
+    var viewPager: ViewPager? = null
+    var tabLayout: TabLayout? = null
+    var pageAdapter: PageAdapter? = null
+    var fragmentList: MutableList<Fragment> = ArrayList()
+    var titles: MutableList<String> = ArrayList()
+    var rootView: View? = null //缓存Fragment view，防止页面切换造成数据清除
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.eye_fragment, container, false)
-        val viewPager = view.findViewById<ViewPager2>(R.id.Viewpager)
-        val TabLayout = view.findViewById<TabLayout>(R.id.tab)
+        viewPager = view.findViewById(R.id.Viewpager)
+        tabLayout = view.findViewById(R.id.tab)
+
         return view
     }
 
@@ -29,6 +39,22 @@ class Eye_Fragment : Fragment() {
 
     //初始化控件
     private fun initWidget(){
+        val listframent1 = List_Fragment()
+        val listframent2 = List_Fragment()
+        fragmentList.add(listframent1)
+        fragmentList.add(listframent2)
+        titles.add("视力检测")
+        titles.add("视力恢复")
 
+        //将数据展示在页面上
+        //tabLayout!!.setupWithViewPager(viewPager)
+        pageAdapter = PageAdapter(
+            fragmentManager,
+            fragmentList,
+            titles
+        )
+        viewPager!!.adapter = pageAdapter
+        //viewPager!!.offscreenPageLimit = 2 //设置预加载页面当fragment切换时数据不会消失，但是应用会闪退
     }
 }
+
